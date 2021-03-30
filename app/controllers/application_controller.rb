5da1a43020f993fd2c126be615d33d2e5b54234f
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-03-31 02:15:14 +0900

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
 
   private
+
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:name_family_zen,:name_first_zen,:name_family_kana, :name_first_kana, :birth])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :name_family_zen, :name_first_zen, :name_family_kana, :name_first_kana,
+                                             :birth])
   end
 end
