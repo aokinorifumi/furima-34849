08144162dcf1df3855e9b6200b21496722a1a5af
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-03-31 02:07:03 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_ZENKANA_REGEX = /\A[ァ-ヶー－]+\z/
+
+  
   validates :nickname, presence: true
-  validates :name_family_zen, presence: true
-  validates :name_first_zen, presence: true
-  validates :name_family_kana, presence: true
-  validates :name_first_kana, presence: true
+  validates :name_family_zen, presence: true, format: { with: VALID_ZENKAKU_REGEX}
+  validates :name_first_zen, presence: true, format: { with: VALID_ZENKAKU_REGEX}
+  validates :name_family_kana, presence: true, format: { with: VALID_ZENKANA_REGEX}
+  validates :name_first_kana, presence: true, format: { with: VALID_ZENKANA_REGEX}
   validates :birth, presence: true
+
+  VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/
+  validates :password, presence: true, length: { minimum: 6}, format: { with: VALID_PASSWORD_REGEX}
+
+
 
 end
