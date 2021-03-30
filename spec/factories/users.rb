12087b60cb3707664bfdb629e6335b5547cf43b8
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-03-31 02:07:24 +0900

    テストコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'aaa000'}
+    password_confirmation {password}
+    name_family_zen       {'阿笠'}
+    name_first_zen        {'博士'}
+    name_family_kana      {'アガサ'}
+    name_first_kana       {'ヒロシ'}
+    birth                 {'1930-01-01'}
+  end
+end
