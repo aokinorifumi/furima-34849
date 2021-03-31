2eeb2d4530ef3e87ec46fd8554eab7eb47042269
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-03-31 12:09:13 +0900

    コード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_ZENKAKU_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
   VALID_ZENKANA_REGEX = /\A[ァ-ヶー－]+\z/
-
-  validates :nickname, presence: true
-  validates :name_family_zen, presence: true, format: { with: VALID_ZENKAKU_REGEX }
-  validates :name_first_zen, presence: true, format: { with: VALID_ZENKAKU_REGEX }
-  validates :name_family_kana, presence: true, format: { with: VALID_ZENKANA_REGEX }
-  validates :name_first_kana, presence: true, format: { with: VALID_ZENKANA_REGEX }
-  validates :birth, presence: true
-
   VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+
+  with_options presence: true do
+    validates :nickname
+    validates :name_family_zen, format: { with: VALID_ZENKAKU_REGEX }
+    validates :name_first_zen, format: { with: VALID_ZENKAKU_REGEX }
+    validates :name_family_kana, format: { with: VALID_ZENKANA_REGEX }
+    validates :name_first_kana, format: { with: VALID_ZENKANA_REGEX }
+    validates :birth
+    validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  end
 end
