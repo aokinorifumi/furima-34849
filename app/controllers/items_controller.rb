66059d39f9b269ef242e20f31d973f8b28e7271a
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-04-02 16:30:23 +0900

    出品フォームの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :status_id, :burden_id, :prefecture_id, :delivery_day_id, :price ).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,32 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :burden_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :delivery_id, numericality: { other_than: 1 }
+    validates :price
+    validates :image
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :status_id
+  belongs_to :burden_id
+  belongs_to :prefecture_id
+  belongs_to :delivery_day_id
+
+
+
+  
+
+
+
+
+
 end
