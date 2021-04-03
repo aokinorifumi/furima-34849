ba0c8c307a4ab504626d3ae3e5eb001bf5ca59f6
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-04-03 14:52:37 +0900

    バリデーション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
   end
 
@@ -8,10 +9,15 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :text, :category_id, :status_id, :burden_id, :prefecture_id, :delivery_day_id, :price ).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :burden_id, :prefecture_id, :delivery_id, :price ).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,17 +10,16 @@ class Item < ApplicationRecord
     validates :burden_id, numericality: { other_than: 1 }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :delivery_id, numericality: { other_than: 1 }
-    validates :price
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :image
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id
-  belongs_to :status_id
-  belongs_to :burden_id
-  belongs_to :prefecture_id
-  belongs_to :delivery_day_id
-
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :delivery
 
 
   
