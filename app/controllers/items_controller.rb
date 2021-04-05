c1e6d4d5f50f2b8e62e0daea7b3cda5dc9ed2876
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-04-05 14:22:39 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :user_commit, only: [:edit,:update]
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -18,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user == @item.user
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -41,4 +40,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :burden_id, :prefecture_id, :delivery_id,
                                  :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def user_commit
+    redirect_to root_path unless current_user == @item.user
+  end
 end
+
