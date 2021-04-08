0a4550b71618d40deb32481af662db79c56cd7df
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-04-08 15:37:53 +0900

    コントローラー設定

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,12 +1,13 @@
 class SalesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+
 
   def index
-    @item = Item.find(params[:item_id])
     @sale_street = SaleStreet.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @sale_street = SaleStreet.new(sale_params)
     if @sale_street.valid?
       pay_item
@@ -18,6 +19,11 @@ class SalesController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user.id || @item.sale.present?
+  end
 
   def sale_params
     params.require(:sale_street).permit(:postal_cord, :prefecture_id, :area, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
