f1fcc5ffd09a613b905ab6bd9a5e05308d4bf516
aokinorifumi <n.aoki.time.limit.tenyears@gmail.com>
2021-04-08 13:19:54 +0900

    バリデーション修正

--- app/models/sale_street.rb
+++ app/models/sale_street.rb
@@ -1,15 +1,16 @@
 class SaleStreet
   include ActiveModel::Model
-  attr_accessor :postal_cord, :prefecture_id, :area, :address, :building_name, :phone_number, :sale_id, :user_id, :item_id
+  attr_accessor :postal_cord, :prefecture_id, :area, :address, :building_name, :phone_number, :sale_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_cord
-    validates :prefecture_id
+    validates :postal_cord, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :area
     validates :address
     validates :phone_number
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
